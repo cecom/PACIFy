585e5817edf056f54b8f33e07f16bf273dcc571f
Sven Oppermann <sven.oppermann@gmail.com>
2016-10-31 09:31:01 +0100

    command added

--- impl/src/main/java/com/geewhiz/pacify/Replacer.java
+++ impl/src/main/java/com/geewhiz/pacify/Replacer.java
@@ -8,7 +8,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * 
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -132,7 +132,7 @@ public class Replacer {
         FilterManager filterManager = new FilterManager(entityManager, propertyResolveManager);
 
         LinkedHashSet<Defect> defects = new LinkedHashSet<Defect>();
-        defects.addAll(filterManager.doFilter());
+            defects.addAll(filterManager.doFilter());
 
         return defects;
     }
