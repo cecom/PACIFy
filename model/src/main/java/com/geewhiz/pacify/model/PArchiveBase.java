ae4d8e33b7ee98612f67c83e2b897670bf181026
Sven Oppermann <sven.oppermann@gmail.com>
2017-01-02 13:36:54 +0100

    licence info, some cleanup

--- model/src/main/java/com/geewhiz/pacify/model/PArchiveBase.java
+++ model/src/main/java/com/geewhiz/pacify/model/PArchiveBase.java
@@ -1,10 +1,29 @@
+/*-
+ * ========================LICENSE_START=================================
+ * com.geewhiz.pacify.model
+ * %%
+ * Copyright (C) 2011 - 2017 Sven Oppermann
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
+
 package com.geewhiz.pacify.model;
 
 import java.io.File;
 
 import javax.xml.bind.Unmarshaller;
 
-import org.apache.commons.compress.archivers.ArchiveStreamFactory;
 import org.jvnet.jaxb2_commons.lang.CopyStrategy;
 import org.jvnet.jaxb2_commons.lang.EqualsStrategy;
 import org.jvnet.jaxb2_commons.lang.HashCodeStrategy;
@@ -12,25 +31,6 @@ import org.jvnet.jaxb2_commons.lang.ToStringStrategy;
 import org.jvnet.jaxb2_commons.locator.ObjectLocator;
 
 import com.geewhiz.pacify.defect.DefectRuntimeException;
-
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 
 public abstract class PArchiveBase {
 
@@ -41,12 +41,7 @@ public abstract class PArchiveBase {
      * The physical representation. if this entry is an archive in an archive.
      */
     private File     file;
-
-    // TODO: liegt jetzt in archiveutils, muss raus
-    public String getInternalType() {
-        int idx = getRelativePath().lastIndexOf(".");
-        return getRelativePath().substring(idx + 1);
-    }
+    private String   archiveType;
 
     public PMarker getPMarker() {
         if (isArchiveFile())
@@ -82,26 +77,12 @@ public abstract class PArchiveBase {
         this.pMarker = pMarker;
     }
 
-    // TODO: liegt jetzt in archiveutils, muss raus
     public String getType() {
-        String type = getInternalType();
+        return archiveType;
+    }
 
-        if ("jar".equalsIgnoreCase(type)) {
-            return ArchiveStreamFactory.JAR;
-        }
-        if ("war".equalsIgnoreCase(type)) {
-            return ArchiveStreamFactory.JAR;
-        }
-        if ("ear".equalsIgnoreCase(type)) {
-            return ArchiveStreamFactory.JAR;
-        }
-        if ("zip".equalsIgnoreCase(type)) {
-            return ArchiveStreamFactory.ZIP;
-        }
-        if ("tar".equalsIgnoreCase(type)) {
-            return ArchiveStreamFactory.TAR;
-        }
-        return type;
+    public void setType(String archiveType) {
+        this.archiveType = archiveType;
     }
 
     public String getBeginToken() {
